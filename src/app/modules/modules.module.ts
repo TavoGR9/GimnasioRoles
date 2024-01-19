afa72a2eb5376c0923782459eaa78c293857722d
roghelio <roghelio.soi.io@live.com.ar>
2024-01-19 13:06:14 -0600

    librerias ok

--- src/app/modules/modules.module.ts
+++ src/app/modules/modules.module.ts
@@ -5,6 +5,33 @@ import { ModulesRoutingModule } from './modules-routing.module';
 import { HomeComponent } from './home/home.component';
 import { ReportsComponent } from './reports/reports.component';
 
+//Modulos Material
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
+import { MatSlideToggleModule, _MatSlideToggleRequiredValidatorModule} from '@angular/material/slide-toggle';
+
+//Modulos fontawesome
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faPaypal } from '@fortawesome/free-brands-svg-icons';
+import { faCheckCircle, faCircleUser, faCreditCard } from '@fortawesome/free-regular-svg-icons';
+import { faPowerOff } from '@fortawesome/free-solid-svg-icons';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -13,7 +40,43 @@ import { ReportsComponent } from './reports/reports.component';
   ],
   imports: [
     CommonModule,
-    ModulesRoutingModule
+    ModulesRoutingModule,
+    FontAwesomeModule,
+    ReactiveFormsModule,
+    FormsModule,
+
+    MatToolbarModule, //barra de navegacion
+    MatDialogModule, // dialogos emergentes
+    MatSlideToggleModule,
+    _MatSlideToggleRequiredValidatorModule,
+    MatRadioModule,//radio botones
+    MatButtonToggleModule,
+    MatCardModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    MatButtonModule,
+    MatTabsModule, // tabs de material (pestañas)
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatMenuModule,
+    MatDividerModule,
+    MatListModule,
+    
   ]
 })
-export class ModulesModule { }
+export class ModulesModule {
+  constructor(library: FaIconLibrary) {
+    library.addIcons(
+      faCircleUser,
+      faCreditCard,
+      faPaypal,
+      faCheckCircle,
+      faPowerOff
+    );
+  }
+ }
